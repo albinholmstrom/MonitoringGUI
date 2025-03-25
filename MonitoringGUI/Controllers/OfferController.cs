46c3404d2400002a22584213937d6c57256ee9e7
AlexAhmanHV <alexander.ahman@student.hv.se>
2025-03-25 10:04:01 +0100

    Event och offers added

---
+++ MonitoringGUI/Controllers/OfferController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitoringGUI.Controllers
+{
+    public class OfferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
